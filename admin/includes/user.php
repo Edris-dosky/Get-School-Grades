a56b3849567d3c94a896ada2790f372ec01e8371
Edris <edris.dosky@gmail.com>
2023-01-02 19:35:16 +0300

    create get by is function

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -8,6 +8,10 @@ class User{
        return self::query_proccess("SELECT * FROM `student` ");
     }
     
+    public static function get_user_by_id($userid){
+        $single_user_data = self:: query_proccess("SELECT * FROM `user` WHERE `id` = '$userid'");
+        return !empty($single_user_data) ? array_shift($single_user_data) : false ;
+    }
 
     public static function query_proccess($sql){
         global $db; 
