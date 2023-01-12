427cd3d13fbeb6f6b52449f837a5f698acda381a
Edris <edris.dosky@gmail.com>
2023-01-12 21:52:48 +0300

    improve design navbar

--- admin/includes/nav.php
+++ admin/includes/nav.php
@@ -9,34 +9,34 @@ require_once('init.php');
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/Get-School-Grades/admin/assets/css/output.css">
+    <link href="/your-path-to-fontawesome/css/fontawesome.css" rel="stylesheet">
     <title>A+</title>
 </head>
-<body class="font-display">
+<body class="font-display bg-zinc-100">
 
-<nav class=" bg-slate-500 w-full h-14 ">
-<div class="container mx-auto flex justify-between items-center h-full">
-  <a href="" class="flex justify-between items-center w-36">
-    <div class=""><p>A+</p></div>
-    <img src="/admin/assets/img/logo.jpg" alt="" class="h-12 w-12 object-cover ">
+<nav class=" bg-blue-500 w-full h-14 ">
+<div class="container mx-auto flex justify-between items-center h-full flex-row-reverse">
+  <a href="" class="flex justify-around items-center w-36">
+    <p class="text-center px-4 pb-1.5 pt-2 font-bold bg-orange-500 text-2xl rounded-full">A<sup>+</sup> </p>
   </a>
+
 <ul class="flex justify-around items-center w-2/3 flex-row-reverse ">
   <li>
-    <a href="">سەرەتا</a>
+    <a href="" class="link-nav">سەرەتا</a>
   </li>
   <li>
-    <a href="">قوتابیاکان</a>
+    <a href="" class="link-nav">قوتابیاکان</a>
   </li>
   <li>
-    <a href="">نمرەکان</a>
+    <a href="" class="link-nav">نمرەکان</a>
   </li>
   <li>
-    <a href="">هەژمار</a>
+    <a href="" class="link-nav">هەژمار</a>
   </li>
   <li>
-    <a href="">دەربارە</a>
+    <a href="" class="link-nav">دەربارە</a>
   </li>
-  
 </ul>
+<button class="bg-red-400 px-4 py-1.5 text-center rounded-xl active:translate-y-1 hover:bg-red-500">چونەدەر</button>
 </div>
 </nav>
-<img  alt="">
