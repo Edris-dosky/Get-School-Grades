288f0776e8f796cabb1e90be5daef5a71afeae65
Edris <edris.dosky@gmail.com>
2023-01-27 01:25:51 +0300

    fix some problem

--- admin/home.php
+++ admin/home.php
@@ -9,11 +9,3 @@ echo $_SESSION['id'];
 
 
 <?php require_once('includes/footer.php'); ?>
-<div class="flex flex-col justify-center items-center bg-black-rgba p-8">
-        <h3 class="">
-            Edris Dosky 
-        </h3>
-        <p>
-            I am web developer i live soran and i can make every system that you want
-        </p>
-    </div>
--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -1,1 +1,9 @@
 
+<div class="flex flex-col justify-center items-center bg-black-rgba p-8">
+    <h3 class="">
+        Edris Dosky 
+    </h3>
+    <p>
+        I am web developer i live soran and i can make every system that you want
+    </p>
+</div>
