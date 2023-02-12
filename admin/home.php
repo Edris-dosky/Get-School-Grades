3fd433874c33d3aebabd522f349806073c67cc67
Edris <edris.dosky@gmail.com>
2023-02-12 22:18:04 +0300

    get all data

--- admin/home.php
+++ admin/home.php
@@ -3,14 +3,14 @@
 
 <?php
 echo $_SESSION['id'];
-
+$all_data=api::get_all(0);
 ?>
 <div class=" container mx-auto bg-blue-400 h-30 flex items-center justify-evenly h-40" >
 <div class="border border-black">
     <p>edris</p>
 </div>
 <div class="">
-    easter_days
+    
 </div>
 </div>
 
